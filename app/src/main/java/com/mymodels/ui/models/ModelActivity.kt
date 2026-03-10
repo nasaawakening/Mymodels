a3ea54481821f46821f3b4d78e91dfaf1625f2b8
nasaawakening <nasaawakening@gmail.com>
2026-03-10 14:33:26 +0700

    Update ModelActivity.kt

--- app/src/main/java/com/mymodels/ui/models/ModelActivity.kt
+++ app/src/main/java/com/mymodels/ui/models/ModelActivity.kt
@@ -1,43 +1,20 @@
 package com.mymodels.ui.models
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mymodels.R
-import com.mymodels.ui.chat.ChatActivity
-import kotlinx.android.synthetic.main.activity_models.*
 
-class ModelsActivity : AppCompatActivity() {
+class ModelActivity : AppCompatActivity() {
 
-    private lateinit var adapter: ModelAdapter
+    private lateinit var modelsList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_models)
 
-        adapter = ModelAdapter { model ->
-
-            val intent = Intent(this, ChatActivity::class.java)
-            intent.putExtra("model", model)
-
-            startActivity(intent)
-        }
-
+        modelsList = findViewById(R.id.modelsList)
         modelsList.layoutManager = LinearLayoutManager(this)
-        modelsList.adapter = adapter
-
-        loadModels()
-    }
-
-    private fun loadModels() {
-
-        val models = listOf(
-            "llama-3-gguf",
-            "mistral-gguf",
-            "phi-2-gguf"
-        )
-
-        adapter.submit(models)
     }
 }
