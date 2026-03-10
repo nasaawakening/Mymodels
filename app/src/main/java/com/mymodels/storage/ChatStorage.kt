da1aa7e92c2d976290e8a925ba91e7a2c7178057
nasaawakening <nasaawakening@gmail.com>
2026-03-10 15:34:35 +0700

    Create storage/ChatStorage.kt

---
+++ app/src/main/java/com/mymodels/storage/ChatStorage.kt
@@ -0,0 +1,33 @@
+package com.mymodels.storage
+
+import android.content.Context
+import com.google.gson.Gson
+import com.mymodels.data.ChatSession
+import java.io.File
+
+object ChatStorage {
+
+    private val gson = Gson()
+
+    fun save(context: Context, sessions: List<ChatSession>) {
+
+        val json = gson.toJson(sessions)
+
+        val file = File(context.filesDir, "chats.json")
+
+        file.writeText(json)
+    }
+
+    fun load(context: Context): MutableList<ChatSession> {
+
+        val file = File(context.filesDir, "chats.json")
+
+        if (!file.exists()) return mutableListOf()
+
+        val json = file.readText()
+
+        val type = Array<ChatSession>::class.java
+
+        return gson.fromJson(json, type).toMutableList()
+    }
+}
