bfeb3eec7e313811763c4c939da3b0cb73fef1c9
nasaawakening <nasaawakening@gmail.com>
2026-03-10 15:10:46 +0700

    Create settings/SettingsActivity.kt

---
+++ app/src/main/java/com/mymodels/ui/settings/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.mymodels.ui.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mymodels.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+    }
+}
