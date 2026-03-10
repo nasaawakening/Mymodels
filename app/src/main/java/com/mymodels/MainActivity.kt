2cdb4c404e3a900c94c61672c3ea690458e3cea1
nasaawakening <nasaawakening@gmail.com>
2026-03-10 14:59:31 +0700

    Update MainActivity.kt

--- app/src/main/java/com/mymodels/MainActivity.kt
+++ app/src/main/java/com/mymodels/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       
+        NotificationHelper.createChannel(this)
 
         val modelsButton = findViewById<Button>(R.id.modelsButton)
 
