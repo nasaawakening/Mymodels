b31f80047c029d5e1a9cb50711f3687ddafb15d1
nasaawakening <nasaawakening@gmail.com>
2026-03-12 06:03:40 +0700

    Create ModelManagerActivity.kt

---
+++ app/src/main/java/com/mymodels/ui/models/ModelManagerActivity.kt
@@ -0,0 +1,14 @@
+package com.mymodels.ui.model
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mymodels.R
+
+class ModelManagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_model_manager)
+    }
+
+}
