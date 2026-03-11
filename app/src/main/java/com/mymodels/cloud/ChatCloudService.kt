11122bbc6a381de13f97bf8593f6bcd286ad6a11
nasaawakening <nasaawakening@gmail.com>
2026-03-12 04:36:07 +0700

    Create cloud/ChatCloudService.kt

---
+++ app/src/main/java/com/mymodels/cloud/ChatCloudService.kt
@@ -0,0 +1,22 @@
+package com.mymodels.cloud
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.FirebaseAuth
+import com.mymodels.models.ChatMessage
+
+object ChatCloudService {
+
+    private val db = FirebaseFirestore.getInstance()
+
+    fun saveMessage(message: ChatMessage) {
+
+        val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
+
+        db.collection("users")
+            .document(uid)
+            .collection("chats")
+            .add(message)
+
+    }
+
+}
