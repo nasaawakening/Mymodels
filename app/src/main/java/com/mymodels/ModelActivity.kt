32c35838523985a94ea846734be17bddbdba2520
nasaawakening <nasaawakening@gmail.com>
2026-03-11 12:51:40 +0700

    Create ModelActivity.kt

---
+++ app/src/main/java/com/mymodels/ModelActivity.kt
@@ -0,0 +1,13 @@
+package com.mymodels
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ModelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_model)
+    }
+}
