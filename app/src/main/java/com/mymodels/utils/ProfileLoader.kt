85dc3c0159f28cd2c8f6e2f1bf5611d1e8251352
nasaawakening <nasaawakening@gmail.com>
2026-03-12 04:34:11 +0700

    Create ProfileLoader.kt

---
+++ app/src/main/java/com/mymodels/utils/ProfileLoader.kt
@@ -0,0 +1,23 @@
+package com.mymodels.profile
+
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+
+object ProfileLoader {
+
+    fun load(avatar: ImageView, name: TextView, email: TextView) {
+
+        val user = FirebaseAuth.getInstance().currentUser ?: return
+
+        name.text = user.displayName
+        email.text = user.email
+
+        Glide.with(avatar.context)
+            .load(user.photoUrl)
+            .into(avatar)
+
+    }
+
+}
