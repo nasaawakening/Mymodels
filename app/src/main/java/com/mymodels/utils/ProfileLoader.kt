272d266a4bba5a6366cb6c2bf32a064d967755f0
nasaawakening <nasaawakening@gmail.com>
2026-03-12 09:24:32 +0700

    Update ProfileLoader.kt

--- app/src/main/java/com/mymodels/utils/ProfileLoader.kt
+++ app/src/main/java/com/mymodels/utils/ProfileLoader.kt
@@ -1,22 +1,18 @@
 package com.mymodels.utils
 
 import android.widget.ImageView
-import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
 
 object ProfileLoader {
 
-    fun load(avatar: ImageView, name: TextView, email: TextView) {
+    fun loadAvatar(imageView: ImageView, url: String?) {
 
-        val user = FirebaseAuth.getInstance().currentUser ?: return
+        if (url == null) return
 
-        name.text = user.displayName
-        email.text = user.email
-
-        Glide.with(avatar.context)
-            .load(user.photoUrl)
-            .into(avatar)
+        Glide.with(imageView.context)
+            .load(url)
+            .circleCrop()
+            .into(imageView)
 
     }
 
