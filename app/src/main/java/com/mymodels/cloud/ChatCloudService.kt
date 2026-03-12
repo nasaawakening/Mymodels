6055a89f406904b81193f637789dfbcb4730e153
nasaawakening <nasaawakening@gmail.com>
2026-03-12 09:27:16 +0700

    Update ChatCloudService.kt

--- app/src/main/java/com/mymodels/cloud/ChatCloudService.kt
+++ app/src/main/java/com/mymodels/cloud/ChatCloudService.kt
@@ -1,7 +1,7 @@
 package com.mymodels.cloud
 
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mymodels.models.ChatMessage
 
 object ChatCloudService {
@@ -12,35 +12,10 @@ object ChatCloudService {
 
         val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
 
-        val data = hashMapOf(
-            "text" to message.text,
-            "isUser" to message.isUser,
-            "time" to System.currentTimeMillis()
-        )
-
         db.collection("users")
             .document(uid)
             .collection("chats")
-            .add(data)
-
-    }
-
-    fun loadMessages(callback: (List<ChatMessage>) -> Unit) {
-
-        val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
-
-        db.collection("users")
-            .document(uid)
-            .collection("chats")
-            .orderBy("time")
-            .get()
-            .addOnSuccessListener {
-
-                val list = it.toObjects(ChatMessage::class.java)
-
-                callback(list)
-
-            }
+            .add(message)
 
     }
 
