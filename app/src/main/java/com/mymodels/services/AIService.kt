204e77ca10318c39ee9772d3b3f71c2a378fdb79
nasaawakening <nasaawakening@gmail.com>
2026-03-12 11:06:46 +0700

    Update AIService.kt

--- app/src/main/java/com/mymodels/services/AIService.kt
+++ app/src/main/java/com/mymodels/services/AIService.kt
@@ -1,17 +1,33 @@
 package com.mymodels.services
 
+import com.google.firebase.auth.FirebaseAuth
+
 object AIService {
+
+    fun generate(userName: String, prompt: String): String {
+
+        return """
+Hello $userName 👋
+
+You said:
+
+$prompt
+
+How can I help you today?
+""".trimIndent()
+
+    }
 
     fun sendMessage(prompt: String, callback: (String) -> Unit) {
 
-        val response = """
-Hello 👋
+        val googleName =
+            FirebaseAuth.getInstance()
+                .currentUser?.displayName ?: "User"
 
-You said:
-$prompt
-"""
+        val response = generate(googleName, prompt)
 
         callback(response)
+
     }
 
 }
