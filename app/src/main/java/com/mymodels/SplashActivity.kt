bbec2f644d8b3dd72b483d710db16439a7fc81c0
nasaawakening <nasaawakening@gmail.com>
2026-03-13 15:34:13 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/mymodels/SplashActivity.kt
+++ app/src/main/java/com/mymodels/SplashActivity.kt
@@ -4,40 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.google.firebase.auth.FirebaseAuth
-import com.mymodels.ui.auth.LoginActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIME = 2000L
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_splash)
-
         lifecycleScope.launch {
 
-            delay(SPLASH_TIME)
+            delay(1200) // optional
 
-            try {
+            startActivity(
+                Intent(this@SplashActivity, MainActivity::class.java)
+            )
 
-                val user = FirebaseAuth.getInstance().currentUser
-
-                val next = if (user != null) {
-                    MainActivity::class.java
-                } else {
-                    LoginActivity::class.java
-                }
-
-                startActivity(Intent(this@SplashActivity, next))
-                finish()
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            finish()
         }
     }
 }
