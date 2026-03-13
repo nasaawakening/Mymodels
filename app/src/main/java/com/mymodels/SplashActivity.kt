472e009e9ac579568f4b715d83869f81e46f2a68
nasaawakening <nasaawakening@gmail.com>
2026-03-13 16:50:56 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/mymodels/SplashActivity.kt
+++ app/src/main/java/com/mymodels/SplashActivity.kt
@@ -3,24 +3,19 @@ package com.mymodels
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launch {
+        // ❌ Tidak perlu setContentView
 
-            delay(1200) // optional
+        startActivity(
+            Intent(this, MainActivity::class.java)
+        )
 
-            startActivity(
-                Intent(this@SplashActivity, MainActivity::class.java)
-            )
-
-            finish()
-        }
+        finish()
     }
 }
